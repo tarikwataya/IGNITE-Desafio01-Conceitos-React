48c9b6d755b60e389c95233525cb474ce556e9ad
Tárik Wataya <tarikwataya@gmail.com>
2021-03-09 21:23:54 -0300

    ✔ Challenge Completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(old => [...old, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updateTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+
+    setTasks(updateTasks)
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filterTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filterTasks);
   }
 
   return (
